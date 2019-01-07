e6a257e9130f7c7b5785ba578761a1f52bdacda2
Daniel Shokouhi <dshokouhi@gmail.com>
2019-01-06 17:33:14 -0800

    Persistent map update

--- neato.py
+++ neato.py
@@ -23,6 +23,7 @@ DOMAIN = 'neato'
 NEATO_ROBOTS = 'neato_robots'
 NEATO_LOGIN = 'neato_login'
 NEATO_MAP_DATA = 'neato_map_data'
+NEATO_PERSISTENT_MAPS = 'neato_persisten_maps'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -31,22 +32,29 @@ CONFIG_SCHEMA = vol.Schema({
     })
 }, extra=vol.ALLOW_EXTRA)
 
+STATES = {
+    1: 'Idle',
+    2: 'Busy',
+    3: 'Pause',
+    4: 'Error'
+}
+
 MODE = {
     1: 'Eco',
     2: 'Turbo'
 }
 
 ACTION = {
-    0: 'Invalid',
-    1: 'House Cleaning',
-    2: 'Spot Cleaning',
-    3: 'Manual Cleaning',
+    0: 'No action',
+    1: 'House cleaning',
+    2: 'Spot cleaning',
+    3: 'Manual cleaning',
     4: 'Docking',
-    5: 'User Menu Active',
-    6: 'Suspended Cleaning',
-    7: 'Updating',
-    8: 'Copying logs',
-    9: 'Recovering Location',
+    5: 'User menu active',
+    6: 'Cleaning cancelled',
+    7: 'Updating...',
+    8: 'Copying logs...',
+    9: 'Calculating position...',
     10: 'IEC test',
     11: 'Map cleaning',
     12: 'Exploring map (creating a persistent map)',
@@ -56,68 +64,25 @@ ACTION = {
 }
 
 ERRORS = {
-    'ui_error_battery_battundervoltlithiumsafety': 'Replace battery',
-    'ui_error_battery_critical': 'Replace battery',
-    'ui_error_battery_invalidsensor': 'Replace battery',
-    'ui_error_battery_lithiumadapterfailure': 'Replace battery',
-    'ui_error_battery_mismatch': 'Replace battery',
-    'ui_error_battery_nothermistor': 'Replace battery',
-    'ui_error_battery_overtemp': 'Replace battery',
-    'ui_error_battery_undercurrent': 'Replace battery',
-    'ui_error_battery_undertemp': 'Replace battery',
-    'ui_error_battery_undervolt': 'Replace battery',
-    'ui_error_battery_unplugged': 'Replace battery',
     'ui_error_brush_stuck': 'Brush stuck',
     'ui_error_brush_overloaded': 'Brush overloaded',
     'ui_error_bumper_stuck': 'Bumper stuck',
-    'ui_error_check_battery_switch': 'Check battery',
-    'ui_error_corrupt_scb': 'Call customer service corrupt board',
-    'ui_error_deck_debris': 'Deck debris',
     'ui_error_dust_bin_missing': 'Dust bin missing',
     'ui_error_dust_bin_full': 'Dust bin full',
     'ui_error_dust_bin_emptied': 'Dust bin emptied',
-    'ui_error_hardware_failure': 'Hardware failure',
-    'ui_error_ldrop_stuck': 'Clear my path',
-    'ui_error_lwheel_stuck': 'Clear my path',
-    'ui_error_navigation_backdrop_frontbump': 'Clear my path',
     'ui_error_navigation_backdrop_leftbump': 'Clear my path',
-    'ui_error_navigation_backdrop_wheelextended': 'Clear my path',
     'ui_error_navigation_noprogress': 'Clear my path',
     'ui_error_navigation_origin_unclean': 'Clear my path',
     'ui_error_navigation_pathproblems_returninghome': 'Cannot return to base',
     'ui_error_navigation_falling': 'Clear my path',
-    'ui_error_navigation_noexitstogo': 'Clear my path',
-    'ui_error_navigation_nomotioncommands': 'Clear my path',
-    'ui_error_navigation_rightdrop_leftbump': 'Clear my path',
-    'ui_error_navigation_undockingfailed': 'Clear my path',
     'ui_error_picked_up': 'Picked up',
-    'ui_error_rdrop_stuck': 'Clear my path',
-    'ui_error_rwheel_stuck': 'Clear my path',
     'ui_error_stuck': 'Stuck!',
-    'ui_error_unable_to_see': 'Clean vacuum sensors',
-    'ui_error_vacuum_slip': 'Clear my path',
-    'ui_error_vacuum_stuck': 'Clear my path',
-    'ui_error_warning': 'Error check app',
-    'batt_base_connect_fail': 'Battery failed to connect to base',
-    'batt_base_no_power': 'Battery base has no power',
-    'batt_low': 'Battery low',
-    'batt_on_base': 'Battery on base',
-    'clean_tilt_on_start': 'Clean the tilt on start',
     'dustbin_full': 'Dust bin full',
     'dustbin_missing': 'Dust bin missing',
-    'gen_picked_up': 'Picked up',
-    'hw_fail': 'Hardware failure',
-    'hw_tof_sensor_sensor': 'Hardware sensor disconnected',
-    'lds_bad_packets': 'Bad packets',
-    'lds_deck_debris': 'Debris on deck',
-    'lds_disconnected': 'Disconnected',
-    'lds_jammed': 'Jammed',
     'maint_brush_stuck': 'Brush stuck',
     'maint_brush_overload': 'Brush overloaded',
     'maint_bumper_stuck': 'Bumper stuck',
-    'maint_customer_support_qa': 'Contact customer support',
     'maint_vacuum_stuck': 'Vacuum is stuck',
-    'maint_vacuum_slip': 'Vacuum is stuck',
     'maint_left_drop_stuck': 'Vacuum is stuck',
     'maint_left_wheel_stuck': 'Vacuum is stuck',
     'maint_right_drop_stuck': 'Vacuum is stuck',
@@ -125,15 +90,7 @@ ERRORS = {
     'not_on_charge_base': 'Not on the charge base',
     'nav_robot_falling': 'Clear my path',
     'nav_no_path': 'Clear my path',
-    'nav_path_problem': 'Clear my path',
-    'nav_backdrop_frontbump': 'Clear my path',
-    'nav_backdrop_leftbump': 'Clear my path',
-    'nav_backdrop_wheelextended': 'Clear my path',
-    'nav_mag_sensor': 'Clear my path',
-    'nav_no_exit': 'Clear my path',
-    'nav_no_movement': 'Clear my path',
-    'nav_rightdrop_leftbump': 'Clear my path',
-    'nav_undocking_failed': 'Clear my path'
+    'nav_path_problem': 'Clear my path'
 }
 
 ALERTS = {
@@ -141,11 +98,7 @@ ALERTS = {
     'ui_alert_recovering_location': 'Returning to start',
     'dustbin_full': 'Please empty dust bin',
     'maint_brush_change': 'Change the brush',
-    'maint_filter_change': 'Change the filter',
-    'clean_completed_to_start': 'Cleaning completed',
-    'nav_floorplan_not_created': 'No floorplan found',
-    'nav_floorplan_load_fail': 'No floorplan found',
-    'nav_floorplan_localization_fail': 'Floorplan no longer matches surroundings'
+    'maint_filter_change': 'Change the filter'
 }
 
 
@@ -165,7 +118,7 @@ def setup(hass, config):
     return True
 
 
-class NeatoHub:
+class NeatoHub(object):
     """A My Neato hub wrapper class."""
 
     def __init__(self, hass, domain_config, neato):
@@ -178,6 +131,7 @@ class NeatoHub:
             domain_config[CONF_USERNAME],
             domain_config[CONF_PASSWORD])
         self._hass.data[NEATO_ROBOTS] = self.my_neato.robots
+        self._hass.data[NEATO_PERSISTENT_MAPS] = self.my_neato.persistent_maps
         self._hass.data[NEATO_MAP_DATA] = self.my_neato.maps
 
     def login(self):
@@ -197,6 +151,7 @@ class NeatoHub:
         _LOGGER.debug("Running HUB.update_robots %s",
                       self._hass.data[NEATO_ROBOTS])
         self._hass.data[NEATO_ROBOTS] = self.my_neato.robots
+        self._hass.data[NEATO_PERSISTENT_MAPS] = self.my_neato.persistent_maps
         self._hass.data[NEATO_MAP_DATA] = self.my_neato.maps
 
     def download_map(self, url):
